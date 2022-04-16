a3c8d203ee43232739abc8b9e4e880773e0f24b9
jojoBodhiCode <Jordan.Neumann@gmail.com>
2022-04-16 10:01:56 -0400

    @jsneum01: bootstrap import, background and logo implementation responsive, favicon replacement,

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,30 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import background from '../src/images/bg-galaxy.jpg';
+import jojoBodhiCodeLogo from '../src/images/jojoBodhiCodeLogo.png';
 
 export default function Home() {
+
   return (
-   <div>
-     Hello World
+    <>
+      <style>
+        {
+          `
+          .bg-galaxy{
+            background-image:  url(${background.src});
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;
+          }
+          
+          .h-vh-100 {
+            height: 100vh;
+          }`
+        }
+      </style>
+   <div className="page-wrapper bg-galaxy w-100 h-vh-100">
+     <div className="max-w-">
+      <img className="img-fluid w-100" src={jojoBodhiCodeLogo.src} alt="jojoBodhiCodeLogo"/>
+     </div>
    </div>
+   </>
   )
 }
