91c51782ec54977c3fd9a9729d3944353dd6ae4a
jojoBodhiCode <Jordan.Neumann@gmail.com>
2022-04-17 00:57:01 -0400

    @jsneum01: fixed prettier linter and linted files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
-import background from '../src/images/bg-galaxy.jpg';
-import jojoBodhiCodeLogo from '../src/images/jojoBodhiCodeLogo.png';
+import background from '../src/images/bg-galaxy.jpg'
+import jojoBodhiCodeLogo from '../src/images/jojoBodhiCodeLogo.png'
 
 export default function Home() {
-
-  return (
-    <>
-      <style>
-        {
-          `
+    return (
+        <>
+            <style>
+                {`
           .bg-galaxy{
             background-image:  url(${background.src});
             background-size: cover;
@@ -17,14 +15,34 @@ export default function Home() {
           
           .h-vh-100 {
             height: 100vh;
-          }`
-        }
-      </style>
-   <div className="page-wrapper bg-galaxy w-100 h-vh-100">
-     <div className="max-w-">
-      <img className="img-fluid w-100" src={jojoBodhiCodeLogo.src} alt="jojoBodhiCodeLogo"/>
-     </div>
-   </div>
-   </>
-  )
+          }
+          
+          .max-w-75-percent { 
+            max-width: 75%;
+          }
+          
+          .bg-001219 {
+            background-color: #001219;
+          }
+          `}
+            </style>
+            <div className="page-wrapper bg-galaxy w-100 h-vh-100">
+                <div className="w-100 d-flex">
+                    <img
+                        className="img-fluid w-100 max-w-75-percent p-5 mx-auto"
+                        src={jojoBodhiCodeLogo.src}
+                        alt="jojoBodhiCodeLogo"
+                    />
+                </div>
+
+                <div className="d-flex flex-column align-items-center">
+                    <h1 className="text-white pb-3">
+                        <strong>jojoBodhiCode</strong>
+                    </h1>
+                </div>
+
+                <div className="main-body-wrapper bg-001219w-100"></div>
+            </div>
+        </>
+    )
 }
