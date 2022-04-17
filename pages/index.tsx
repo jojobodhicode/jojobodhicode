a308cfa67cfac21c2a477ec205dcf942ad9ce3c2
jojoBodhiCode <Jordan.Neumann@gmail.com>
2022-04-17 01:05:28 -0400

    @jsneum01: more prettier changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import background from '../src/images/bg-galaxy.jpg'
-import jojoBodhiCodeLogo from '../src/images/jojoBodhiCodeLogo.png'
+import background from "../src/images/bg-galaxy.jpg"
+import jojoBodhiCodeLogo from "../src/images/jojoBodhiCodeLogo.png"
 
 export default function Home() {
-    return (
-        <>
-            <style>
-                {`
+  return (
+    <>
+      <style>
+        {`
           .bg-galaxy{
             background-image:  url(${background.src});
             background-size: cover;
@@ -24,25 +24,29 @@ export default function Home() {
           .bg-001219 {
             background-color: #001219;
           }
+
+          .text-shadow {
+            text-shadow: 2px 2px #ff0000;
+          }
           `}
-            </style>
-            <div className="page-wrapper bg-galaxy w-100 h-vh-100">
-                <div className="w-100 d-flex">
-                    <img
-                        className="img-fluid w-100 max-w-75-percent p-5 mx-auto"
-                        src={jojoBodhiCodeLogo.src}
-                        alt="jojoBodhiCodeLogo"
-                    />
-                </div>
+      </style>
+      <div className="page-wrapper bg-galaxy w-100 h-vh-100">
+        <div className="w-100 d-flex">
+          <img
+            className="img-fluid w-100 max-w-75-percent p-5 mx-auto"
+            src={jojoBodhiCodeLogo.src}
+            alt="jojoBodhiCodeLogo"
+          />
+        </div>
 
-                <div className="d-flex flex-column align-items-center">
-                    <h1 className="text-white pb-3">
-                        <strong>jojoBodhiCode</strong>
-                    </h1>
-                </div>
+        <div className="d-flex flex-column align-items-center">
+          <h1 className="text-white pb-3 text-shadow">
+            <strong>jojoBodhiCode</strong>
+          </h1>
+        </div>
 
-                <div className="main-body-wrapper bg-001219w-100"></div>
-            </div>
-        </>
-    )
+        <div className="main-body-wrapper bg-001219w-100"></div>
+      </div>
+    </>
+  )
 }
